621887a3ba727366a51bb37499ac36eff64aab14
Nicole Montano <nicolysis@gmail.com>
2015-10-15 20:47:11 -0500

    Now with more parameter acceptance.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strconv"
 )
 
 var workingDir string = path.Join(os.TempDir(), "apollo-audio")
@@ -49,7 +50,7 @@ func downloadFromS3AndSave(filename string) string {
 	out, err := os.Create(clipPath)
 	check(err)
 	defer out.Close()
-	resp, err := http.Get("http://austinpray.s3.amazonaws.com/static/apolloclips/" + filename)
+	resp, err := http.Get("http://exploreapollo-tmp.s3.amazonaws.com/audio/" + filename)
 	check(err)
 	defer resp.Body.Close()
 	_, err = io.Copy(out, resp.Body)
@@ -97,10 +98,81 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("done")
 }
 
+func thyme(timecode string) int {
+	// TODO: validate that it's in expected range, ie. that time exists
+
+	var startSecond int
+
+	// days, err := strconv.Atoi(timecode[0:3])
+	// hrs, err := strconv.Atoi(timecode[3:5])
+	min, err := strconv.Atoi(timecode[5:7])
+	sec, err := strconv.Atoi(timecode[7:9])
+
+	if err != nil {
+		panic(err)
+	}
+
+	startSecond = min * 60 + sec // lol math
+
+	return startSecond
+
+}
+
+func streamHandler(w http.ResponseWriter, r *http.Request) {
+	// .../stream?track=14_SPACE-ENVIRONMENT_20July_20-07-00&track=16_SPAN_20July_20-07-00&track=18_BOOSTER-C_20July_20-07-00&format=mp3&t=201200700
+	// TODO: validate query ie. filename.wav and filename.trs has to exist, format is mp3 or ogg, t is DDDHHMMSS
+
+	var audioFiles []string
+	//var trsFiles []string
+	r.ParseForm()
+
+	tracks := r.Form["track"]
+	for n := range tracks {
+		tmpStr1 := fmt.Sprintf("%s.wav", tracks[n])
+		// tmpStr2 := fmt.Sprintf("%s.trs", tracks[n])
+		fp := downloadFromS3AndSave(tmpStr1)
+		audioFiles = append(audioFiles, fp)
+		//append(trsFiles, downloadFromS3AndSave(tmpStr2))
+	}
+
+	format := r.Form["format"][0]
+	timecode := r.Form["t"][0]
+	// TODO: convert MET timecode to start second in appropriate file
+	fmt.Fprintf(w, "format: %s\n", format)
+	fmt.Fprintf(w, "timecode: %s, startsecond: %d\n", timecode, thyme(timecode))
+
+	// mmmmmmagic
+	sox, err := exec.LookPath("sox")
+	check(err)
+	fmt.Println("using sox " + sox)
+	ffmpeg, err := exec.LookPath("ffmpeg")
+	check(err)
+	fmt.Println("using ffmpeg " + ffmpeg)
+	soxArgs := []string{"-t", "wav", "-m"}
+	soxArgs = append(soxArgs, audioFiles...)
+	soxArgs = append(soxArgs, "-p")
+	soxCommand := exec.Command(sox, soxArgs...)
+	ffmpegArgs := []string{"-i", "-", "-f", format, "-ab", "256k", "pipe:"}
+	ffmpegCommand := exec.Command(ffmpeg, ffmpegArgs...)
+	fw := flushWriter{w: w}
+	if f, ok := w.(http.Flusher); ok {
+		fw.f = f
+	}
+	ffmpegCommand.Stdin, _ = soxCommand.StdoutPipe()
+	ffmpegCommand.Stdout = &fw
+	ffmpegCommand.Stderr = os.Stdout
+	ffmpegCommand.Start()
+	soxCommand.Run()
+	ffmpegCommand.Wait()
+	fmt.Println("done")
+}
+
+
 func main() {
 	makeDir(workingDir)
 	makeDir(clipDir)
-	http.HandleFunc("/stream.mp3", handler)
+	//http.HandleFunc("/stream.mp3", handler)
+	http.HandleFunc("/stream", streamHandler)
 	ServerPort := "5000" // default port
 	if len(os.Getenv("PORT")) > 0 {
 		ServerPort = os.Getenv("PORT")
