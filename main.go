486906042f7f297ab7bde12b091bd5ced15b2476
Nicole Montano <nicolemontano@gmail.com>
2015-10-17 22:27:34 -0500

    mixes according to format parameter
    aac yields m4a, ogg yields ogg, anything else yields mp3. to be handled
    differently.

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 
 var workingDir string = path.Join(os.TempDir(), "apollo-audio")
 var clipDir string = path.Join(workingDir, "clips")
+var AAC string = "aac"
+var OGG string = "ogg"
 
 func check(e error) {
 	if e != nil {
@@ -99,42 +101,36 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func thyme(timecode string) int {
-	// TODO: validate that it's in expected range, ie. that time exists
-
+	// TODO: validate: in expected range, ie. that time exists
 	var startSecond int
-
 	// days, err := strconv.Atoi(timecode[0:3])
 	// hrs, err := strconv.Atoi(timecode[3:5])
 	min, err := strconv.Atoi(timecode[5:7])
 	sec, err := strconv.Atoi(timecode[7:9])
-
 	if err != nil {
 		panic(err)
 	}
-
 	startSecond = min * 60 + sec // lol math
-
 	return startSecond
 
 }
 
 func streamHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "audio/mpeg")
 	// .../stream?track=14_SPACE-ENVIRONMENT_20July_20-07-00&track=16_SPAN_20July_20-07-00&track=18_BOOSTER-C_20July_20-07-00&format=aac&t=201200700
-	// TODO: validate query ie. filename.wav and filename.trs has to exist, format is aac or ogg, t is DDDHHMMSS
+	// TODO: validate: filename.wav, filename.trs exist
+	// TODO: validate: format is aac or ogg
+	// TODO: validate: t is DDDHHMMSS
 
 	var audioFiles []string
-	//var trsFiles []string
 	r.ParseForm()
 
 	tracks := r.Form["track"]
 	for n := range tracks {
 		tmpStr1 := fmt.Sprintf("%s.wav", tracks[n])
-		// tmpStr2 := fmt.Sprintf("%s.trs", tracks[n])
-		// fp := downloadFromS3AndSave(tmpStr1)
-		fp := fmt.Sprintf("%s", tmpStr1)
-		fmt.Println("Pretending to download " + fp)
+		fmt.Println("Pretending to download " + tmpStr1)
+		fp := downloadFromS3AndSave(tmpStr1)
 		audioFiles = append(audioFiles, fp)
-		//append(trsFiles, downloadFromS3AndSave(tmpStr2))
 	}
 
 	format := r.Form["format"][0]
@@ -144,22 +140,54 @@ func streamHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "timecode: %s, startsecond: %d\n", timecode, thyme(timecode))
 
 	// mmmmmmagic
+	sox, err := exec.LookPath("sox")
+	check(err)
+	fmt.Println("using sox " + sox)
+	ffmpeg, err := exec.LookPath("ffmpeg")
+	check(err)
+	fmt.Println("using ffmpeg " + ffmpeg)
+	
 	// there's probably a better way to do this. halp.
 	soxArgs := []string{"-t", "wav", "-m"}
 	soxArgs = append(soxArgs, audioFiles...)
 	soxArgs = append(soxArgs, "-p")
-	fmt.Println(soxArgs)
-
-	ffmpegArgs := []string{"-i", "-", "-f", format, "-ab", "256k", "pipe:"}
-	fmt.Println(ffmpegArgs)
-
+	soxCommand := exec.Command(sox, soxArgs...)
+	
+	var ffmpegArgs []string
+	if format == AAC {
+		// ffmpegArgs = []string{"-i", "-", "-strict", "2", "-c:a", "aac", "-b:a", "240k", "-f", "m4a", "pipe:"}
+		ffmpegArgs = []string{"-i", "-", "-c:a", "libfdk_aac", "-b:a", "256k", "-f", "m4a", "pipe:"}
+		// dued idek wut encoder
+		// works, but gotta compile ffmpeg with special options
+	} else if format == OGG {
+		ffmpegArgs = []string{"-i", "-", "-c:a", "libvorbis", "-qscale:a", "6", "-f", "ogg", "pipe:"}
+	} else {
+		fmt.Println("unsupported output format requested")
+		// break it
+		ffmpegArgs = []string{"-i", "-", "-f", "mp3", "-ab", "256k", "pipe:"}
+	}
+	ffmpegCommand := exec.Command(ffmpeg, ffmpegArgs...)
+	
+	fw := flushWriter{w: w}
+	if f, ok := w.(http.Flusher); ok {
+		fw.f = f
+	}
+	
+	ffmpegCommand.Stdin, _ = soxCommand.StdoutPipe()
+	ffmpegCommand.Stdout = &fw
+	ffmpegCommand.Stderr = os.Stdout
+		
+	ffmpegCommand.Start()
+	soxCommand.Run()
+	ffmpegCommand.Wait()
+	
+	fmt.Println("done")
 }
 
 
 func main() {
 	makeDir(workingDir)
 	makeDir(clipDir)
-	//http.HandleFunc("/stream.mp3", handler)
 	http.HandleFunc("/stream", streamHandler)
 	ServerPort := "5000" // default port
 	if len(os.Getenv("PORT")) > 0 {
