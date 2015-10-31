e451372ad9ec740dbf04c7c25eff9f7558c2c937
Nicole Montano <nicolemontano@gmail.com>
2015-10-31 05:48:26 -0500

    get file location from db
    only not really. mock return.
    will actually be getting the full url from db - accomodate this
    also added mechanism for stitching together channel tracks in case request
    time spans multiple files - still need to test if this works

--- main.go
+++ main.go
@@ -53,6 +53,7 @@ func downloadFromS3AndSave(filename string) string {
 	check(err)
 	defer out.Close()
 	resp, err := http.Get("http://exploreapollo-tmp.s3.amazonaws.com/audio/Tape885_20July_20-07-00_HR2U_LunarLanding/" + filename)
+	// resp, err := http.Get(filename)
 	check(err)
 	defer resp.Body.Close()
 	_, err = io.Copy(out, resp.Body)
@@ -100,24 +101,64 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("done")
 }
 
-func thyme(timecode string) int {
-	// TODO: validate: in expected range, ie. that time exists
-	var startSecond int
-	// days, err := strconv.Atoi(timecode[0:3])
-	// hrs, err := strconv.Atoi(timecode[3:5])
-	min, err := strconv.Atoi(timecode[5:7])
-	sec, err := strconv.Atoi(timecode[7:9])
-	if err != nil {
-		panic(err)
+func getFileLocation(mission int, chanNum int, start int, end int) []string {
+	/*
+		will have to make sure file locations are acquired in order u kno...sheeeeeit
+		my relational algebra is rusty.
+
+		channeldb :=
+		select url, start_met, end_met from channel_chunks
+		where channel = chann
+
+		fileloc1 :=
+		select url, end_met from channeldb
+		where start_met <= start
+		and end_met > start
+
+		if fileloc1.end_met < end:
+
+		fileloc2 :=
+		select url from channeldb
+		where start_met < end
+		and end_met <= end
+
+	*/
+	var urls []string
+	switch chanNum {
+		case 14:
+			urls = append(urls, "14_SPACE-ENVIRONMENT_20July_20-07-00.wav")
+		case 16:
+			urls = append(urls, "16_SPAN_20July_20-07-00.wav")
+		case 18:
+			urls = append(urls, "18_BOOSTER-C_20July_20-07-00.wav")
+		case 19:
+			urls = append(urls, "19_BOOSTER-R_20July_20-07-00.wav")
+		case 20:
+			urls = append(urls, "20_3-FLIGHT-DIRECTOR-LOOP_20July_20-07-00.wav")
+		case 21:
+			urls = append(urls, "21_3-AFD-CONF-LOOP_20July_20-07-00.wav")
+		case 22:
+			urls = append(urls, "22_3-GOSS-2-LOOP_20July_20-07-00.wav")
+		case 23:
+			urls = append(urls, "23_ALSEP-EAO-2_20July_20-07-00.wav")
+		case 24:
+			urls = append(urls, "24_3-MOCR-DYN-LOOP_20July_20-07-00.wav")
+		default:
+			panic("unknown or unavailable channel")
 	}
-	startSecond = min * 60 + sec // lol math
-	return startSecond
+	return urls
+}
+
+func endMET(start int, duration int) int {
+	// assuming met is in milliseconds, duration is given in seconds
+	return start + duration * 1000
 
 }
 
+
+// http://localhost:5000/stream?mission=14&channel=14&channel=18&channel=24&format=aac&t=369300000&len=600
 func streamHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "audio/mpeg")
-	// .../stream?track=14_SPACE-ENVIRONMENT_20July_20-07-00&track=16_SPAN_20July_20-07-00&track=18_BOOSTER-C_20July_20-07-00&format=aac&t=1023255&len=300
 	// TODO: validate: filename.wav, filename.trs exist
 	// TODO: validate: format is aac or ogg
 	// TODO: validate: t is accepted MET HHHDDMM
@@ -127,18 +168,53 @@ func streamHandler(w http.ResponseWriter, r *http.Request) {
 
 	// use tracks parameter to query DB and find where appropriate track(s) live
 	// may need multple tracks of the same channel in case length exceeds end of file MET
-	tracks := r.Form["track"]
-	for n := range tracks {
-		tmpStr1 := fmt.Sprintf("%s.wav", tracks[n])
-		fmt.Println("Pretending to download " + tmpStr1)
-		fp := downloadFromS3AndSave(tmpStr1)
-		audioFiles = append(audioFiles, fp)
-	}
-
+	mission, err := strconv.Atoi(r.Form["mission"][0])
+	check(err)
+	channels := r.Form["channel"]
 	format := r.Form["format"][0]
-	timecode := r.Form["t"][0]
+	start, err := strconv.Atoi(r.Form["t"][0])
+	check(err)
+	duration, err := strconv.Atoi(r.Form["len"][0])
+	check(err)
+	end := endMET(start, duration)
 	// TODO: convert MET timecode to start second in appropriate file
-	fmt.Fprintf(w, "debug\n -- format: %s --\n -- timecode: %s --\n -- startsecond: %s --", format, timecode, thyme(timecode))
+	fmt.Fprintf(w, "debug\n -- format: %s --\n -- startms: %d --\n -- endms: %d --\n", format, start, end)
+
+	// retrive files to be mixed
+	// TODO: refactoring needed OMG
+	for n := range channels {
+		var chunks []string
+		chanNum, err := strconv.Atoi(channels[n])
+		check(err)
+		output := fmt.Sprintf("channel%d.wav", chanNum)
+		chunkLoci := getFileLocation(mission, chanNum, start, end)
+
+		// queerrry
+		for c := range chunkLoci {
+			fmt.Println("Downloading " + chunkLoci[c])
+			fp := downloadFromS3AndSave(chunkLoci[c])
+			chunks = append(chunks, fp)
+		}
+
+		if len(chunks) == 0 {
+			panic("no corresponding file(s) found. something's wrong.")
+		} else if len(chunks) == 1 {
+			audioFiles = append(audioFiles, chunks[0])
+		} else {
+			sox, err := exec.LookPath("sox")
+			check(err)
+			fmt.Println("using sox " + sox)
+			soxArgs := []string{"-t", "wav", "--combine", "concatenate"}
+			soxArgs = append(soxArgs, chunks...)
+			soxArgs = append(soxArgs, output)
+			soxCommand := exec.Command(sox, soxArgs...)
+
+			soxCommand.Run()
+
+			audioFiles = append(audioFiles, output)
+		}
+
+	}
 
 	// mmmmmmagic
 	sox, err := exec.LookPath("sox")
@@ -148,6 +224,7 @@ func streamHandler(w http.ResponseWriter, r *http.Request) {
 	check(err)
 	fmt.Println("using ffmpeg " + ffmpeg)
 
+	// mix the tracks
 	// there's probably a better way to do this. halp.
 	// TODO: time stuff
 	soxArgs := []string{"-t", "wav", "-m"}
@@ -155,6 +232,7 @@ func streamHandler(w http.ResponseWriter, r *http.Request) {
 	soxArgs = append(soxArgs, "-p")
 	soxCommand := exec.Command(sox, soxArgs...)
 
+	// convert the thing
 	var ffmpegArgs []string
 	if format == AAC {
 		// ffmpegArgs = []string{"-i", "-", "-strict", "2", "-c:a", "aac", "-b:a", "240k", "-f", "m4a", "pipe:"}
